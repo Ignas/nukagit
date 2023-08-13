4806d1bfe863074caf0dbee5ed9179b29122b3a0
Ignas Mikalajunas <ignas@pow.lt>
2023-08-14 00:03:14 +0300

    Log repositories being resolved

--- src/main/java/lt/pow/nukagit/DfsRepositoryResolver.java
+++ src/main/java/lt/pow/nukagit/DfsRepositoryResolver.java
@@ -3,10 +3,14 @@ package lt.pow.nukagit;
 import org.eclipse.jgit.internal.storage.dfs.DfsRepositoryDescription;
 import org.eclipse.jgit.internal.storage.dfs.InMemoryRepository;
 import org.eclipse.jgit.lib.Repository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.concurrent.ConcurrentHashMap;
 
 public class DfsRepositoryResolver {
+
+  static private Logger LOGGER = LoggerFactory.getLogger(DfsRepositoryResolver.class);
   private final ConcurrentHashMap<String, Repository> repositoryCache;
 
   public DfsRepositoryResolver() {
@@ -14,6 +18,7 @@ public class DfsRepositoryResolver {
   }
 
   synchronized Repository resolveDfsRepository(String username, String[] args) {
+    LOGGER.debug("resolveDfsRepository: username={}, args={}", username, args);
     return repositoryCache.computeIfAbsent(args[1], (key) -> new InMemoryRepository(new DfsRepositoryDescription(key)));
   }
 }
