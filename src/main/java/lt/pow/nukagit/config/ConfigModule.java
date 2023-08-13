193c6942ff032002b0fad48defc268e76b805492
Ignas Mikalajunas <ignas@pow.lt>
2023-08-14 00:03:16 +0300

    Only accept environment variables prefixed with NUKAGIT_

--- src/main/java/lt/pow/nukagit/config/ConfigModule.java
+++ src/main/java/lt/pow/nukagit/config/ConfigModule.java
@@ -4,12 +4,15 @@ import dagger.Module;
 import dagger.Provides;
 
 import java.nio.file.Path;
+import java.util.List;
+
 import org.github.gestalt.config.Gestalt;
 import org.github.gestalt.config.builder.GestaltBuilder;
 import org.github.gestalt.config.exceptions.GestaltException;
 import org.github.gestalt.config.source.ClassPathConfigSource;
 import org.github.gestalt.config.source.EnvironmentConfigSource;
 import org.github.gestalt.config.source.FileConfigSource;
+import org.github.gestalt.config.tag.Tags;
 
 @Module
 public class ConfigModule {
@@ -24,7 +27,7 @@ public class ConfigModule {
                   new ClassPathConfigSource(
                       "/default.properties")) // Load the default property files from resources.
               .addSource(new FileConfigSource(Path.of("config/application.yaml")))
-              .addSource(new EnvironmentConfigSource())
+              .addSource(new RobustEnvironmentConfigSource("NUKAGIT_", true))
               .build();
       gestalt.loadConfigs();
     } catch (GestaltException e) {
