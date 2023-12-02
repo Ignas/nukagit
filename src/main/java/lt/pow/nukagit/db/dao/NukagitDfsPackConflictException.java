139bd2ab187a9cd6b524e1d94430af35dd6c75e4
Ignas Mikalajunas <ignas@pow.lt>
2023-12-03 00:07:44 +0200

    Make spotbugs happy by inheriting from Exception

--- src/main/java/lt/pow/nukagit/db/dao/NukagitDfsPackConflictException.java
+++ src/main/java/lt/pow/nukagit/db/dao/NukagitDfsPackConflictException.java
@@ -1,6 +1,6 @@
 package lt.pow.nukagit.db.dao;
 
-public class NukagitDfsPackConflictException extends Throwable {
+public class NukagitDfsPackConflictException extends Exception {
     public NukagitDfsPackConflictException(Exception e) {
         super(e);
     }
