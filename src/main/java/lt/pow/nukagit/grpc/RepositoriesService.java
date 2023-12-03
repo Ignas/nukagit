11b47ae1afd4e5a74330aaa530a7327057851d45
Ignas Mikalajunas <ignas@pow.lt>
2023-12-03 19:06:49 +0200

    Reformatted file

--- src/main/java/lt/pow/nukagit/grpc/RepositoriesService.java
+++ src/main/java/lt/pow/nukagit/grpc/RepositoriesService.java
@@ -1,6 +1,7 @@
 package lt.pow.nukagit.grpc;
 
 import io.grpc.stub.StreamObserver;
+
 import javax.inject.Inject;
 import javax.inject.Singleton;
 
@@ -13,40 +14,41 @@ import lt.pow.nukagit.proto.Types;
 @Singleton
 public class RepositoriesService extends RepositoriesServiceGrpc.RepositoriesServiceImplBase {
 
-  private final DfsRepositoryResolver repositoryResolver;
-  private final NukagitDfsDao nukagitDfsDao;
+    private final DfsRepositoryResolver repositoryResolver;
+    private final NukagitDfsDao nukagitDfsDao;
 
-  @Inject
-  public RepositoriesService(DfsRepositoryResolver repositoryResolver, NukagitDfsDao nukagitDfsDao) {
-    this.repositoryResolver = repositoryResolver;
-    this.nukagitDfsDao = nukagitDfsDao;
-  }
-
-  @Override
-  public void listRepositories(
-      Repositories.ListRepositoriesRequest request,
-      StreamObserver<Repositories.ListRepositoriesResponse> responseObserver) {
-    Repositories.ListRepositoriesResponse response =
-        Repositories.ListRepositoriesResponse.newBuilder()
-            .addAllRepositories(
-                repositoryResolver.listRepositories().stream()
-                    .map(
-                        repositoryName ->
-                            Types.Repository.newBuilder().setName(repositoryName).build())
-                    .toList())
-            .build();
-    responseObserver.onNext(response);
-    responseObserver.onCompleted();
-  }
-
-  @Override
-  public void createRepository(Repositories.CreateRepositoryRequest request, StreamObserver<Repositories.CreateRepositoryResponse> responseObserver) {
-    var repositoryName = request.getRepositoryName();
-    if (!repositoryName.startsWith("/")) {
-      repositoryName = "/" + repositoryName;
+    @Inject
+    public RepositoriesService(DfsRepositoryResolver repositoryResolver, NukagitDfsDao nukagitDfsDao) {
+        this.repositoryResolver = repositoryResolver;
+        this.nukagitDfsDao = nukagitDfsDao;
     }
-    nukagitDfsDao.upsertRepository(repositoryName);
-    Repositories.CreateRepositoryResponse response = Repositories.CreateRepositoryResponse.newBuilder().build();
-    responseObserver.onNext(response);
-    responseObserver.onCompleted();  }
+
+    @Override
+    public void listRepositories(
+            Repositories.ListRepositoriesRequest request,
+            StreamObserver<Repositories.ListRepositoriesResponse> responseObserver) {
+        Repositories.ListRepositoriesResponse response =
+                Repositories.ListRepositoriesResponse.newBuilder()
+                        .addAllRepositories(
+                                repositoryResolver.listRepositories().stream()
+                                        .map(
+                                                repositoryName ->
+                                                        Types.Repository.newBuilder().setName(repositoryName).build())
+                                        .toList())
+                        .build();
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
+    }
+
+    @Override
+    public void createRepository(Repositories.CreateRepositoryRequest request, StreamObserver<Repositories.CreateRepositoryResponse> responseObserver) {
+        var repositoryName = request.getRepositoryName();
+        if (!repositoryName.startsWith("/")) {
+            repositoryName = "/" + repositoryName;
+        }
+        nukagitDfsDao.upsertRepository(repositoryName);
+        Repositories.CreateRepositoryResponse response = Repositories.CreateRepositoryResponse.newBuilder().build();
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
+    }
 }
