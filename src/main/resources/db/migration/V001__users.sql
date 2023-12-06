1966b5a5eff9c8b159a00fe01dba75b76ada9647
Ignas Mikalajunas <ignas@pow.lt>
2023-12-06 20:53:15 +0200

    Basic schema for user and public key management

---
+++ src/main/resources/db/migration/V001__users.sql
@@ -0,0 +1,26 @@
+CREATE TABLE users
+(
+    id         CHAR(36) PRIMARY KEY,
+    username   VARCHAR(255) NOT NULL,
+    created_on TIMESTAMP         DEFAULT CURRENT_TIMESTAMP,
+    deleted_on TIMESTAMP    NULL DEFAULT NULL
+);
+
+ALTER TABLE users
+    ADD not_archived BOOLEAN
+        GENERATED ALWAYS AS (IF(deleted_on IS NULL, 1, NULL)) VIRTUAL;
+
+ALTER TABLE users
+    ADD CONSTRAINT UNIQUE (username, not_archived);
+
+CREATE TABLE public_keys
+(
+    id          CHAR(36) PRIMARY KEY,
+    user_id     CHAR(36)  NOT NULL,
+    fingerprint CHAR(32)  NOT NULL,
+    public_key  TEXT      NOT NULL,
+    created_on  TIMESTAMP      DEFAULT CURRENT_TIMESTAMP,
+    deleted_on  TIMESTAMP NULL DEFAULT NULL,
+    FOREIGN KEY (user_id) REFERENCES users (id),
+    UNIQUE (user_id, fingerprint)
+);
