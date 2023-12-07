ff8ef374927744fe29927b88aee1f3155ac1c200
Ignas Mikalajunas <ignas@pow.lt>
2023-12-07 23:01:02 +0200

    Updated gestalt API usage to the latest one

--- src/main/java/lt/pow/nukagit/config/ConfigModule.java
+++ src/main/java/lt/pow/nukagit/config/ConfigModule.java
@@ -5,32 +5,30 @@ import dagger.Provides;
 import org.github.gestalt.config.Gestalt;
 import org.github.gestalt.config.builder.GestaltBuilder;
 import org.github.gestalt.config.exceptions.GestaltException;
-import org.github.gestalt.config.source.ClassPathConfigSource;
-import org.github.gestalt.config.source.EnvironmentConfigSource;
-import org.github.gestalt.config.source.FileConfigSource;
+import org.github.gestalt.config.source.*;
 
 import java.nio.file.Path;
 
 @Module
 public class ConfigModule {
 
-  @Provides
-  Gestalt configurationProvider() {
-    String configPath = System.getProperty("nukagit.config_path", "config/application.yaml");
-    Gestalt gestalt;
-    try {
-      gestalt =
-          new GestaltBuilder()
-              .addSource(
-                  new ClassPathConfigSource(
-                      "/default.properties")) // Load the default property files from resources.
-              .addSource(new FileConfigSource(Path.of(configPath)))
-              .addSource(new EnvironmentConfigSource("NUKAGIT_", true))
-              .build();
-      gestalt.loadConfigs();
-    } catch (GestaltException e) {
-      throw new RuntimeException("Failed to load configuration!", e);
+    @Provides
+    Gestalt configurationProvider() {
+        String configPath = System.getProperty("nukagit.config_path", "config/application.yaml");
+        Gestalt gestalt;
+        try {
+            gestalt =
+                    new GestaltBuilder()
+                            .addSource(
+                                    ClassPathConfigSourceBuilder.builder()
+                                            .setResource("/default.properties").build()) // Load the default property files from resources.
+                            .addSource(FileConfigSourceBuilder.builder().setPath(Path.of(configPath)).build())
+                            .addSource(EnvironmentConfigSourceBuilder.builder().setPrefix("NUKAGIT_").setRemovePrefix(true).build())
+                            .build();
+            gestalt.loadConfigs();
+        } catch (GestaltException e) {
+            throw new RuntimeException("Failed to load configuration!", e);
+        }
+        return gestalt;
     }
-    return gestalt;
-  }
 }
