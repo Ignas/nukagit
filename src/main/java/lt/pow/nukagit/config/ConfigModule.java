1ea912d8b6bf563f6cb7910d7618e398020af7d5
Ignas Mikalajunas <ignas@pow.lt>
2023-12-07 20:07:05 +0200

    Upgraded gestalt, seems like no need to patch it anymore

--- src/main/java/lt/pow/nukagit/config/ConfigModule.java
+++ src/main/java/lt/pow/nukagit/config/ConfigModule.java
@@ -6,6 +6,7 @@ import org.github.gestalt.config.Gestalt;
 import org.github.gestalt.config.builder.GestaltBuilder;
 import org.github.gestalt.config.exceptions.GestaltException;
 import org.github.gestalt.config.source.ClassPathConfigSource;
+import org.github.gestalt.config.source.EnvironmentConfigSource;
 import org.github.gestalt.config.source.FileConfigSource;
 
 import java.nio.file.Path;
@@ -24,7 +25,7 @@ public class ConfigModule {
                   new ClassPathConfigSource(
                       "/default.properties")) // Load the default property files from resources.
               .addSource(new FileConfigSource(Path.of(configPath)))
-              .addSource(new RobustEnvironmentConfigSource("NUKAGIT_", true))
+              .addSource(new EnvironmentConfigSource("NUKAGIT_", true))
               .build();
       gestalt.loadConfigs();
     } catch (GestaltException e) {
