4f88900f63e020556ae3a49addd06009606288a0
Ignas Mikalajunas <ignas@pow.lt>
2023-12-07 19:52:24 +0200

    Close the datasource between database tests

--- src/test/groovy/lt/pow/nukagit/db/DatabaseTestBase.groovy
+++ src/test/groovy/lt/pow/nukagit/db/DatabaseTestBase.groovy
@@ -2,17 +2,12 @@ package lt.pow.nukagit.db
 
 import com.zaxxer.hikari.HikariConfig
 import com.zaxxer.hikari.HikariDataSource
-import lt.pow.nukagit.db.entities.Pack
 import org.flywaydb.core.Flyway
 import org.jdbi.v3.core.Jdbi
-import org.jdbi.v3.core.mapper.immutables.JdbiImmutables
-import org.jdbi.v3.sqlobject.SqlObjectPlugin
 import org.testcontainers.containers.MySQLContainer
 import org.testcontainers.spock.Testcontainers
 import spock.lang.Shared
 import spock.lang.Specification
-
-import javax.sql.DataSource
 
 @Testcontainers
 abstract class DatabaseTestBase extends Specification {
@@ -22,6 +17,7 @@ abstract class DatabaseTestBase extends Specification {
             .withUsername("nukagit")
             .withPassword("nukagit")
 
+    HikariDataSource dataSource
     Jdbi jdbi
 
     void setup() {
@@ -31,7 +27,7 @@ abstract class DatabaseTestBase extends Specification {
         hikariConfig.setPassword(mysql.password)
 
         // Create HikariCP DataSource
-        DataSource dataSource = new HikariDataSource(hikariConfig)
+        dataSource = new HikariDataSource(hikariConfig)
 
         // Create JDBI instance
         jdbi = DatabaseModule.jdbi(dataSource)
@@ -40,5 +36,9 @@ abstract class DatabaseTestBase extends Specification {
         Flyway flyway = Flyway.configure().dataSource(dataSource).cleanDisabled(false).load()
         flyway.clean()
         flyway.migrate()
+    }
+
+    void cleanup() {
+        dataSource.close()
     }
 }
