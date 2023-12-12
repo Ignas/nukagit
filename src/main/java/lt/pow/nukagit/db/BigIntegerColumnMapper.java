36c6866f99d9d093c6236400eee1e78a7a594934
Ignas Mikalajunas <ignas@pow.lt>
2023-12-12 11:30:20 +0200

    Handle nulls when mapping BigInteger

--- src/main/java/lt/pow/nukagit/db/BigIntegerColumnMapper.java
+++ src/main/java/lt/pow/nukagit/db/BigIntegerColumnMapper.java
@@ -9,6 +9,9 @@ import java.sql.SQLException;
 public class BigIntegerColumnMapper implements org.jdbi.v3.core.mapper.ColumnMapper<BigInteger> {
     @Override
     public BigInteger map(ResultSet r, int columnNumber, StatementContext ctx) throws SQLException {
+        if (r.getBytes(columnNumber) == null) {
+            return null;
+        }
         return new BigInteger(r.getBytes(columnNumber));
     }
 }
