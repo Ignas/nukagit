3f6483a37021e743f30cf8e60d5496d1821f2f21
Ignas Mikalajunas <ignas@pow.lt>
2023-12-17 03:55:31 +0200

    Use apache KeyUtils to generate fingerprint

--- src/main/java/lt/pow/nukagit/db/entities/PublicKeyData.java
+++ src/main/java/lt/pow/nukagit/db/entities/PublicKeyData.java
@@ -1,10 +1,10 @@
 package lt.pow.nukagit.db.entities;
 
+import org.apache.sshd.common.config.keys.KeyUtils;
 import org.immutables.value.Value;
 import org.jdbi.v3.core.annotation.JdbiProperty;
 
 import java.security.KeyFactory;
-import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.security.PublicKey;
 import java.security.spec.InvalidKeySpecException;
@@ -37,36 +37,8 @@ public interface PublicKeyData {
         }
     }
 
-    /**
-     * Generate fingerprint from public key
-     * <p>
-     * This function generates a SHA1 fingerprint from the public key.
-     * But the algorithm is different from the openssh fingerprint algorithm.
-     *
-     * @return fingerprint
-     */
     @JdbiProperty(map = false)
     default String fingerprint() {
-        byte[] encoded = key().getEncoded();
-
-        // SHA1
-        MessageDigest md;
-        try {
-            md = MessageDigest.getInstance("SHA1");
-        } catch (NoSuchAlgorithmException e) {
-            throw new RuntimeException(e);
-        }
-        md.update(encoded);
-        byte[] fingerprint = md.digest();
-
-        // format fingerprint
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < fingerprint.length; i++) {
-            sb.append(String.format("%02X", fingerprint[i]));
-            if (i < fingerprint.length - 1) {
-                sb.append(":");
-            }
-        }
-        return sb.toString();
+        return KeyUtils.getFingerPrint(key());
     }
 }
