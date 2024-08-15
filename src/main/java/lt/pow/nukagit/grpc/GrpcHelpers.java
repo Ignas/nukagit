3a671310c8965f8715a0c0a976f3ec37f47da820
Ignas Mikalajunas <ignas@devzero.io>
2024-08-15 23:11:22 +0300

    Log any errors that were not handled by grpc handlers

--- src/main/java/lt/pow/nukagit/grpc/GrpcHelpers.java
+++ src/main/java/lt/pow/nukagit/grpc/GrpcHelpers.java
@@ -3,10 +3,14 @@ package lt.pow.nukagit.grpc;
 import io.grpc.Status;
 import io.grpc.StatusRuntimeException;
 import io.grpc.stub.StreamObserver;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.function.Function;
 
 public class GrpcHelpers {
+    private static final Logger LOGGER = LoggerFactory.getLogger(GrpcHelpers.class);
+
     public static <RQ, RS> void unaryCall(RQ request, StreamObserver<RS> responseObserver, Function<RQ, RS> method) {
         RS response;
         try {
@@ -15,6 +19,7 @@ public class GrpcHelpers {
             responseObserver.onError(e);
             return;
         } catch (Exception e) {
+            LOGGER.error("Error processing request", e);
             responseObserver.onError(Status.INTERNAL.withDescription(e.getMessage()).asRuntimeException());
             return;
         }
